316139f00412226ae10948f14b404cfd3b0e8527
Leoknight22 <leolandcat@gmail.com>
2024-10-17 18:31:54 +0200

    Create game.js

---
+++ public/game.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+// Funzione per inviare una mossa al server
+function playCard(cardId) {
+  socket.emit('playCard', { cardId: cardId });
+}
+
+// Ascolta per le mosse dagli altri giocatori
+socket.on('cardPlayed', (data) => {
+  console.log(`Il giocatore ${data.playerId} ha giocato la carta ${data.cardId}`);
+  // Aggiorna l'interfaccia con la nuova mossa
+});
