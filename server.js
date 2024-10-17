8df73633e915afe2dc46873a9584473cbac08d79
Leoknight22 <leolandcat@gmail.com>
2024-10-17 18:47:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,27 +7,43 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let matchmakingQueue = [];
+
 // Serve i file statici dalla cartella "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Gestisci le connessioni dei giocatori
+// Quando un giocatore si connette
 io.on('connection', (socket) => {
   console.log(`Giocatore connesso: ${socket.id}`);
 
-  // Quando un giocatore gioca una carta
-  socket.on('playCard', (data) => {
-    console.log(`Carta giocata: ${data.cardId} dal giocatore ${socket.id}`);
+  // Quando un giocatore entra in matchmaking
+  socket.on('joinMatchmaking', () => {
+    console.log(`Giocatore ${socket.id} entra in matchmaking`);
+    matchmakingQueue.push(socket.id);
 
-    // Invia la mossa a tutti gli altri giocatori
-    socket.broadcast.emit('cardPlayed', { 
-      cardId: data.cardId, 
-      playerId: socket.id 
-    });
+    // Se c'è già un altro giocatore in coda
+    if (matchmakingQueue.length >= 2) {
+      const player1 = matchmakingQueue.shift();  // Primo giocatore
+      const player2 = matchmakingQueue.shift();  // Secondo giocatore
+
+      // Crea una stanza (room) unica per i due giocatori
+      const roomId = `${player1}-${player2}`;
+      io.to(player1).emit('matchFound', { roomId, opponentId: player2 });
+      io.to(player2).emit('matchFound', { roomId, opponentId: player1 });
+
+      console.log(`Match creato tra ${player1} e ${player2} nella stanza ${roomId}`);
+    }
   });
 
   // Quando un giocatore si disconnette
   socket.on('disconnect', () => {
     console.log(`Giocatore disconnesso: ${socket.id}`);
+
+    // Se il giocatore era in matchmaking, rimuovilo dalla coda
+    const index = matchmakingQueue.indexOf(socket.id);
+    if (index !== -1) {
+      matchmakingQueue.splice(index, 1);
+    }
   });
 });
 
